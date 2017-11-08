9f7cce035aee43b581a036d3fd9521c164246e90
Thiago Colares <thicolares@gmail.com>
2017-11-07 22:26:25 -0300

    Refactor the function-based component into a class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,10 @@
-import React from 'react'; // temos que fazer import, por que no fim das contas o <inpunt /> será uma chamada de método
+import React, { Component } from 'react'; // temos que fazer import, por que no fim das contas o <inpunt /> será uma chamada de método
 
-const SearchBar = () => {
-    return <input />; // React.createElement
-};
+class SearchBar extends Component {
+    render() {
+        return <input />
+    }
+}
 
 export default SearchBar;
 // qualquer código que importar search_bar, terá acesso somente à SearchBar
