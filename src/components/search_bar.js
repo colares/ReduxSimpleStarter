695005dee00fdeb8061e6a5cd200ff7828df596d
Thiago Colares <thicolares@gmail.com>
2017-11-07 22:10:24 -0300

    Create SearchBar component and use it inside another component

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react'; // temos que fazer import, por que no fim das contas o <inpunt /> será uma chamada de método
+
+const SearchBar = () => {
+    return <input />; // React.createElement
+};
+
+export default SearchBar;
+// qualquer código que importar search_bar, terá acesso somente à SearchBar
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+// SearchBar poderia ser qualquer nome!
+// Quem diz o que SearchBard é aqui, é o export de search_bar
+
 // (1) Create a new component
 // This component should produce some HTML
+// {process.env.API_KEY}
 const App = () => {
-    return <div>{process.env.API_KEY}</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
 // (2) Take this component's generated HTML and put it on the page (in the DOM)
